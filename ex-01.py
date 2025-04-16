4300fd9d285e1fd010c2d8947cf5c915dee05711
luizgostoso02 <luizpaulorangel98@gmail.com>
2025-04-15 21:13:10 -0300

    Create ex-01.py

---
+++ ex-01.py
@@ -0,0 +1,4 @@
+#1. Frase na tela - Implemente um programa que imprima na tela a frase "O primeiro programa a gente nunca esquece!".
+print("LUIZ PAULO")
+print("RIO DE JANEIRO")
+print("RIO DE JANEIRO")
