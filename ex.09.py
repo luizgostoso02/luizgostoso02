db67338bc6b958b545180d76b6fb7bd85350c539
luizgostoso02 <luizpaulorangel98@gmail.com>
2025-04-29 21:22:37 -0300

    Create ex.09.py

---
+++ ex.09.py
@@ -0,0 +1,4 @@
+numero1 = float(input("Digite o segundo número:"))
+numero2 = float(input("Digite o segundo número: "))
+soma = numero1 + numero2
+print(f"A soma de {numero1} e {numero2} é {soma}.")
