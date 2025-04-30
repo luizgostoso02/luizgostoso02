3730ade63db61bc0b5c62cb952c0b341a5861454
luizgostoso02 <luizpaulorangel98@gmail.com>
2025-04-29 21:45:41 -0300

    Create ex.13.py

---
+++ ex.13.py
@@ -0,0 +1,5 @@
+anos = int(input("Digite a idade em anos: "))
+meses = int(input("Digite a idade em meses: "))
+dias = int(input("Digite a idade em dias: "))
+total_dias = (anos * 365) + (meses * 30) + dias
+print(f"A idade total em dias é: {total_dias} dias.")
