44e2feccbffa938288c20c246c25a1d81f48d5db
luizgostoso02 <luizpaulorangel98@gmail.com>
2025-04-29 21:35:58 -0300

    Create ex.10.py

---
+++ ex.10.py
@@ -0,0 +1,6 @@
+nota1 = float(input("Digite a nota do 1º bimestre: "))
+nota2 = float(input("Digite a nota do 2º bimestre: "))
+nota3 = float(input("Digite a nota do 3º bimestre: "))
+nota4 = float(input("Digite a nota do 4º bimestre: "))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print(f"A média aritmética do aluno é: {media:.2f}")
